999d2c0bc5963613f16138356201b15a6ba0d449
Jack Clayton <jackos@me.com>
2022-12-28 19:32:19 +0700

    Add Concatenation of fields

--- examples/nested_spans.rs
+++ examples/nested_spans.rs
@@ -2,15 +2,14 @@ use tracing::Instrument;
 
 async fn add(a: i32, b: i32) {
     // Important! Don't put any `.await` calls in between `entered()` and `exit()`
-    let _span = tracing::info_span!("add", a, b).entered();
+    let span = tracing::info_span!("add", a, b).entered();
     tracing::info!("result: {}", a + b);
+    span.exit();
 }
 
 #[tokio::main]
 async fn main() {
-	traceon::builder().file(true).on();
-
-	tracing::info!("wow that's cool");
-    let span = tracing::info_span!("math functions", package_name = env!("CARGO_PKG_NAME"));
+    traceon::on();
+    let span = tracing::info_span!("math_functions", package_name = env!("CARGO_PKG_NAME"));
     add(5, 10).instrument(span).await;
 }
